7a4cffe041183b9c47e725dc23c9a7a782e4f0db
vadymHome <vadymgergel@gmail.com>
2025-10-19 20:26:03 +0300

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,17 +5,49 @@ export default defineNuxtConfig({
 
   srcDir: "src/",
 
+  app:{
+    head:{
+      title: "Breez",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "96x96",
+          href: "/favicon/favicon-96x96.png",
+        },
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon/favicon.svg",
+        },
+        {
+          rel: "shortcut icon",
+          href: "/favicon/favicon.ico",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        {
+          rel: "manifest",
+          href: "/favicon/site.webmanifest",
+        },
+      ],
+    }
+  },
+
   css: ["~/assets/scss/common.scss"],
 
   modules: [
     "@nuxt/image",
     "@nuxtjs/google-fonts",
   ],
-googleFonts: {
-  families: {
-    Mohave: true,
+  googleFonts: {
+    families: {
+      Mohave: true,
+    },
+    display: "swap",
+    preload: true,
   },
-  display: "swap",
-  preload: true,
-},
 });
