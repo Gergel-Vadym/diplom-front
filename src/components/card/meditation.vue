1f9d1504a2c10b2a9c56bef462036e1098327e8d
vadymHome <vadymgergel@gmail.com>
2025-10-20 08:12:48 +0300

    pagination meditation

---
+++ src/components/card/meditation.vue
@@ -0,0 +1,77 @@
+<script setup>
+
+  const card = ref([
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+    {
+      img:"./images/meditation.jpg",
+      title:"test",
+      subtitle:"test test test test test",
+    },
+  ])
+
+</script>
+<template>
+  <div v-for="(card, index) in card" :key="`card-meditation-${index}`" class="card card--meditation">
+    <img
+      :scr="card.img"
+      :alt="card.title"
+    />
+    <div class="card__info">
+      <div class="card__title">
+        {{ card.title }}
+      </div>
+      <div class="card__subtitle">
+        {{ card.subtitle }}
+      </div>
+    </div>
+  </div>
+</template>
