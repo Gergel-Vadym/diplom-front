dcd7348d5258eba344c30fba107e1d2e9b32e064
Vadym <vadymgergel@gmail.com>
2025-10-21 22:56:03 +0300

    login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,8 +1,13 @@
 <script setup>
-
 </script>
 
 <template>
-    <main class="main">sign-in</main>
-  
+  <main class="main">
+    <div class="container">
+      <div class="login">
+        <h1 class="login__title">Вхід</h1>
+        <FormSignIn/>
+      </div>
+    </div>
+  </main>
 </template>
