03490963293bc114e3c8b5ee83fd5e91c1f799eb
Vadym <vadymgergel@gmail.com>
2025-10-22 19:35:29 +0300

    analit

--- src/pages/analytics.vue
+++ src/pages/analytics.vue
@@ -1,6 +1,34 @@
 <script setup>
+import Slider from '@vueform/slider'
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+const date = ref();
+
+const form = ref({
+
+})
 </script>
 
 <template>
-  <main class="main">analytics</main>
+  <main class="main">
+    <div class="container">
+      <div class="analytics">
+        <div class="analytics__block">
+          <h2 class="analytics__block-title">Календар настрою</h2>
+          <VueDatePicker
+            v-model="date"
+            :enable-time-picker="false"
+            inline="true"
+          />
+        </div>
+        <div class="analytics__block">
+          <h2 class="analytics__block-title">Календар настрою</h2>
+          <vee-form class="analytics__form"> 
+            <div class=""></div>
+          </vee-form>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
