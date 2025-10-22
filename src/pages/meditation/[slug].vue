f5d118fa7183223a768e10bfb46ed1a6c5c5a084
Vadym <vadymgergel@gmail.com>
2025-10-22 23:33:39 +0300

    meditation

--- src/pages/meditation/[slug].vue
+++ src/pages/meditation/[slug].vue
@@ -2,5 +2,29 @@
 </script>
 
 <template>
-  <main class="main">meditation [slug]</main>
+  <main class="main">
+    <div class="container">
+      <div class="meditation">
+        <div class="meditation__video-wrapper">
+           <!-- <video src="" class="meditation__video"></video> -->
+
+           <NuxtImg
+            src="./images/meditation/meditation1.jpg"
+            class="meditation__video"
+            width="1720"
+            height="900"
+           />
+         </div>
+        <div class="meditation__wrapper">
+          <h1 class="meditation__title">test</h1>
+          <div class="meditation__subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, cum
+            corrupti veritatis asperiores neque perferendis praesentium maxime
+            ipsa eligendi, architecto assumenda numquam quibusdam illo autem
+            quas iste. Consequuntur, saepe quidem.
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
